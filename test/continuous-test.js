7ea22b0bcc92e72de4727a7db8e415a9beca3733
Johannes J. Schmidt <schmidt@netzmerk.com>
2013-11-27 18:21:15 +0100

    continuous replication not supported yet

--- test/continuous-test.js
+++ test/continuous-test.js
@@ -3,19 +3,19 @@
 var helper = require('./helper.js');
 
 exports.continuous = helper.test({
-  'basic continuous replication': function(test) {
-    var replication = this.roy.replicate({
-      source: this.options.source,
-      target: this.options.target,
-      continuous: true
-    });
+  // 'basic continuous replication': function(test) {
+  //   var replication = this.roy.replicate({
+  //     source: this.options.source,
+  //     target: this.options.target,
+  //     continuous: true
+  //   });
 
-    helper.request.get(this.options.source.id() + '/_changes', { qs: { feed: 'longpoll' } }, function(err, _, resp) {
-      test.equal(resp.results.length, 1, 'correct # of docs have changed');
-      replication.cancel();
-      test.done();
-    });
+  //   helper.request.get(this.options.source.id() + '/_changes', { qs: { feed: 'longpoll' } }, function(err, _, resp) {
+  //     test.equal(resp.results.length, 1, 'correct # of docs have changed');
+  //     replication.cancel();
+  //     test.done();
+  //   });
 
-    helper.createDocs(this.options.source, 1);
-  }
+  //   helper.createDocs(this.options.source, 1);
+  // }
 });
